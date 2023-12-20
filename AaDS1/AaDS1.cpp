73765cb91287fd8e07f27b887d5700e6c78412c6
Dasha <tsyganovadasha04@gmail.com>
2023-12-20 05:50:56 +0400

    улучшен вывод, замена типа шаблонным

--- AaDS1/AaDS1.cpp
+++ AaDS1/AaDS1.cpp
@@ -1,19 +1,21 @@
 ﻿#include <iostream>
 
+using T = double;
 class Vector {
 private:
-	int size;
-	int* coordinate;
+	//size_t помещает максимальный возможный размер на данной архитектуре, не переполняясь
+	size_t size;
+	T* coordinate;
 public:
 	Vector() {
 		size = 0;
 		coordinate[0] = 0;
 	}
 
-	Vector(int n, int value) {
+	Vector(size_t n, T value) {
 		size = n;
-		coordinate = new int[size];
-		for (int i = 0; i < size; ++i)
+		coordinate = new T[size];
+		for (size_t i = 0; i < size; ++i)
 			coordinate[i] = value;
 	}
 
@@ -21,8 +23,12 @@ public:
 		delete[] coordinate;
 	}
 	void print() const {
-		for (int i = 0; i < size; ++i)
-			std::cout << coordinate[i] << " ";
+		std::cout << "{ ";
+		for (size_t i = 0; i < size - 1; ++i)
+			std::cout << coordinate[i] << "; ";
+		std::cout << coordinate[size - 1] << " ";
+		std::cout << "}";
+		std::cout << std::endl;
 	}
 };
 int main() {
