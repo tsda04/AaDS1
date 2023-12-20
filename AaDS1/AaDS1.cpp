4877d1b5b4640833b8f669ebeccf03c5caaf7dfb
hp <hp@DESKTOP-DUERNR6>
2023-12-20 05:35:37 +0400

    Добавьте файлы проекта.

---
+++ AaDS1/AaDS1.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+
+class Vector {
+private:
+	int size;
+	int* coordinate;
+public:
+	Vector() {
+		size = 0;
+		coordinate[0] = 0;
+	}
+
+	Vector(int n, int value) {
+		size = n;
+		coordinate = new int[size];
+		for (int i = 0; i < size; ++i)
+			coordinate[i] = value;
+	}
+
+	~Vector() {
+		delete[] coordinate;
+	}
+	void print() const {
+		for (int i = 0; i < size; ++i)
+			std::cout << coordinate[i] << " ";
+	}
+};
+int main() {
+	Vector A(2, 1);
+	A.print();
+	return 0;
+}
