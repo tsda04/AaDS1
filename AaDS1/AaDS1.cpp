54f3ba96b87ef28436be85591d754ec8bcf07507
Dasha <tsyganovadasha04@gmail.com>
2023-12-20 07:00:15 +0400

    добавлены конструктор копирования(+приватный метод), оператор присваивания и метод очищения дин памяти, улучшен конструктор по умолчанию

--- AaDS1/AaDS1.cpp
+++ AaDS1/AaDS1.cpp
@@ -3,36 +3,85 @@
 using T = double;
 class Vector {
 private:
-	//size_t помещает максимальный возможный размер на данной архитектуре, не переполняясь
+	//size_t помещает максимально возможный размер массива на данной архитектуре, не переполняясь
 	size_t size;
 	T* coordinate;
+	void copy(const Vector& other) {
+		this->size = other.size;
+		this->coordinate = new T[size];
+		for (int i = 0; i < size; ++i) {
+			this->coordinate[i] = other.coordinate[i];
+		}
+	}
+	void clear() {
+		delete[] this->coordinate;
+	}
+
 public:
+	//конструктор по умолчанию
 	Vector() {
-		size = 0;
+		size = 1;
+		coordinate = new T[size];
 		coordinate[0] = 0;
 	}
 
+	/*Vector(параметры)
+	{
+		//TODO Конструктор случайных чисел
+
+	}*/
+
+	//конструктор с параметрами
 	Vector(size_t n, T value) {
+		//+ проверка на n = 0
 		size = n;
 		coordinate = new T[size];
 		for (size_t i = 0; i < size; ++i)
 			coordinate[i] = value;
 	}
 
-	~Vector() {
-		delete[] coordinate;
+	//конструктор копирования
+	Vector(const Vector& other) {
+		this->copy(other);
 	}
+
+	//оператор присваивания
+	Vector& operator=(const Vector& other) {
+		if (this != &other) {
+			this->clear();
+			this->copy(other);
+		}
+		return *this;
+	}
+
+	//деструктор
+	~Vector() {
+		clear();
+	}
+
+	//метод печати
 	void print() const {
 		std::cout << "{ ";
+
 		for (size_t i = 0; i < size - 1; ++i)
 			std::cout << coordinate[i] << "; ";
 		std::cout << coordinate[size - 1] << " ";
+
 		std::cout << "}";
 		std::cout << std::endl;
 	}
 };
+
+
 int main() {
-	Vector A(2, 1);
-	A.print();
+
+	{
+		Vector A(2, 1);
+		A.print();
+		Vector B(2, 2);
+		B.print();
+		B = A;
+		B.print();
+	}
 	return 0;
 }
