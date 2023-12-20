c6c8a6a2a5e50983a33627360630e83dd9622f41
Dasha <tsyganovadasha04@gmail.com>
2023-12-20 07:53:07 +0400

    сделаны все TODO: конструткор для заполнения вектора рандомными числами, методы сложения, вычитания, умножения, деления на число; добавлены гетеры для чтения и изменения коорд по индексу и исключения

--- AaDS1/AaDS1.cpp
+++ AaDS1/AaDS1.cpp
@@ -1,4 +1,6 @@
 ﻿#include <iostream>
+#include <random>
+#include <clocale>
 
 using T = double;
 class Vector {
@@ -16,6 +18,13 @@ private:
 	void clear() {
 		delete[] this->coordinate;
 	}
+	int random(int low, int high)
+	{
+		std::random_device rd;
+		std::mt19937 generator(rd());
+		std::uniform_int_distribution<> distribution(low, high);
+		return  distribution(generator);
+	}
 
 public:
 	//конструктор по умолчанию
@@ -25,12 +34,13 @@ public:
 		coordinate[0] = 0;
 	}
 
-	/*Vector(параметры)
+	Vector(size_t n, T start, T end)
 	{
-		//TODO Конструктор случайных чисел
-
-	}*/
-
+		size = n;
+		coordinate = new T[size];
+		for (size_t i = 0; i < size; ++i)
+			coordinate[i] = random(start, end);//2 предупреждения
+	}
 	//конструктор с параметрами
 	Vector(size_t n, T value) {
 		//+ проверка на n = 0
@@ -70,44 +80,123 @@ public:
 		std::cout << "}";
 		std::cout << std::endl;
 	}
+	//сложение векторов
 	void sum(const Vector& other)
 	{
-		//if (разный размер)
-		//TODO сложение векторов (сразу в this)
+		if (size = other.size)
+		{
+			for (size_t i = 0; i < size; ++i)
+			{
+				coordinate[i] += other.coordinate[i];
+			}
+		}
+		throw  std::string{ "The sizes are not equal!" };
 	}
 
+	//вычитание векторов
 	void diff(const Vector& other)
 	{
-		//if (разный размер)
-		//TODO вычитание векторов (сразу в this)
-	}
+		if (size != other.size)
+		{
+			throw std::string{ "The sizes are not equal!" };
+		}
 
+		for (size_t i = 0; i < size; ++i)
+		{
+			coordinate[i]  -= other.coordinate[i];
+		}
+	}
+	//произведение
 	void mult(const Vector& other)
 	{
-		//TODO произведение
+		if (size != other.size)
+		{
+			throw std::string{ "The sizes are not equal!" };
+		}
+
+		for (size_t i = 0; i < size; ++i)
+		{
+			coordinate[i] = coordinate[i] * other.coordinate[i];
+		}
 	}
 
-	void mult(/*число*/)
+	//умножения на число
+	void mult(T n)
 	{
-		//TODO умножения на число
+		for (size_t i = 0; i < size; ++i)
+		{
+			coordinate[i] *= n;
+		}
 	}
 
-	void div(/*число*/)
+	//деление на число
+	void div(T n)
 	{
-		//TODO деление на число
+		if (n)
+		{
+			for (size_t i = 0; i < size; ++i)
+			{
+				coordinate[i] = coordinate[i] / n;
+			}
+		}
+	}
+	T get_coord(int i) const
+	{
+		return coordinate[i];
+	}
+
+	T get_coord(int i)//для изменения
+	{
+		return coordinate[i];
 	}
 };
 
 
 int main() {
-
+	setlocale(LC_ALL, "RUS");
 	{
-		Vector A(2, 1);
-		A.print();
-		Vector B(2, 2);
-		B.print();
-		B = A;
-		B.print();
+		Vector v1(5, 2.1);
+		v1.print();
+
+		Vector v2(v1);
+		Vector v3 = v1;
+
+		Vector v4(3, 5, 10);
+		Vector v5(2, 5, 10);
+		Vector v6(2, 1, 5);
+		Vector v7(2, 1, 10);
+
+		std::cout << "v4: ";
+		v4.print();
+		std::cout << "index 2:" << v4.get_coord(2) << std::endl;
+		std::cout << std::endl;
+		std::cout << "v5:" << std::endl;
+		v5.print();
+		std::cout << "v7:" << std::endl;
+		v7.print();
+		std::cout << "v6:" << std::endl;
+		v6.print();
+
+		try
+		{
+
+			v5.diff(v6);
+			//v7.sum(v6);
+			v7.mult(v6);
+
+			v4.mult(v6);//бросит исключение
+		}
+		catch (std::string error_message)
+		{
+			std::cout << error_message << std::endl;
+		}
+		std::cout << "same v4:" << std::endl;
+		v4.print();
+		std::cout << "v5-v6=";
+		v5.print();
+		std::cout << "v7*v6=";
+		v7.print();
 	}
+
 	return 0;
 }
